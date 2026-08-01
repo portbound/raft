f46cf807916f60f66b0829c7fa42195917138e4e
portbound <jake@portbound.io>
2026-08-01 16:14:34 -0400

    stubbing out election logic

--- internal/node/node.go
+++ internal/node/node.go
@@ -2,6 +2,8 @@ package node
 
 import (
 	"context"
+	"math/rand/v2"
+	"time"
 )
 
 type role int
@@ -11,6 +13,8 @@ const (
 	Candidate
 	Follower
 )
+
+const MaxRPCTimeout = 300 * time.Millisecond
 
 type Peer interface {
 	AppendEntries(ctx context.Context, req *AppendEntriesReq) (*AppendEntriesResp, error)
@@ -70,11 +74,13 @@ type Node struct {
 	role               role
 	currentTerm        uint64
 	votedFor           string
+	electionTimer      *time.Timer
 	log                []*LogEntry
 	commitIndex        uint64
 	lastApplied        uint64
 	requestVoteCalls   chan *requestVoteCall
 	appendEntriesCalls chan *appendEntriesCall
+	electionResults    chan uint64
 }
 
 func New(id string, cluster map[string]Peer) *Node {
@@ -89,12 +95,22 @@ func (n *Node) Submit(ctx context.Context, b []byte) error {
 }
 
 func (n *Node) Run() {
+	n.role = Follower
+	// TODO n.currentTerm = 1 (do we need to do this?)
+	n.electionTimer = newElectionTimer()
+
 	for {
 		select {
 		case call := <-n.appendEntriesCalls:
 			call.results <- n.appendEntries(call.req)
 		case call := <-n.requestVoteCalls:
 			call.results <- n.requestVote(call.req)
+		case <-n.electionTimer.C:
+			n.beginElection()
+		case term := <-n.electionResults:
+			if n.currentTerm == term {
+				// TODO time to lead
+			}
 		}
 	}
 }
@@ -107,8 +123,8 @@ func (n *Node) HandleAppendEntries(ctx context.Context, req *AppendEntriesReq) *
 	}
 
 	select {
-	case resp := <-results:
-		return resp
+	case res := <-results:
+		return res
 	case <-ctx.Done():
 		return nil
 	}
@@ -129,40 +145,46 @@ func (n *Node) HandleRequestVote(ctx context.Context, req *RequestVoteReq) *Requ
 	}
 }
 
-func (n *Node) appendEntries(request *AppendEntriesReq) *AppendEntriesResp {
+func (n *Node) appendEntries(req *AppendEntriesReq) *AppendEntriesResp {
+	if req.Term > n.currentTerm {
+		n.currentTerm = req.Term
+		n.role = Follower
+		n.votedFor = ""
+	}
+
 	response := AppendEntriesResp{
 		Term:    n.currentTerm,
 		Success: false,
 	}
 
 	// 1. Reply false if term < currentTerm (§5.1)
-	if request.Term < n.currentTerm {
+	if req.Term < n.currentTerm {
 		return &response
 	}
 
 	// 2. Reply false if n.log doesn’t contain an entry at request.PrevLogIndex whose term matches request.PrevLogTerm (§5.3)
-	if request.PrevLogIndex > 0 {
-		if int(request.PrevLogIndex) > len(n.log) {
+	if req.PrevLogIndex > 0 {
+		if int(req.PrevLogIndex) > len(n.log) {
 			return &response
 		}
 
-		prevEntry := n.log[request.PrevLogIndex-1]
-		if prevEntry.Term != request.PrevLogTerm {
+		prevEntry := n.log[req.PrevLogIndex-1]
+		if prevEntry.Term != req.PrevLogTerm {
 			return &response
 		}
 	}
 
 	// 3. If an existing entry conflicts with a new one (same index but different terms), delete the existing entry and all that follow it (§5.3)
 	var newEntries []*LogEntry
-	for i, e := range request.Entries {
+	for i, e := range req.Entries {
 		if int(e.Index) > len(n.log) { // we know that this must be +=1 from the last entry in n.log because of the 2nd clause
-			newEntries = request.Entries[i:]
+			newEntries = req.Entries[i:]
 			break
 		}
 
 		if e.Term != n.log[e.Index-1].Term {
 			n.log = n.log[:e.Index-1]
-			newEntries = request.Entries[i:]
+			newEntries = req.Entries[i:]
 			break
 		}
 	}
@@ -171,26 +193,21 @@ func (n *Node) appendEntries(request *AppendEntriesReq) *AppendEntriesResp {
 	n.log = append(n.log, newEntries...)
 
 	// 5. If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
-	if request.LeaderCommit > n.commitIndex {
+	if req.LeaderCommit > n.commitIndex {
 		lastIndex := uint64(len(n.log))
-		if len(request.Entries) > 0 {
-			lastIndex = request.Entries[len(request.Entries)-1].Index
+		if len(req.Entries) > 0 {
+			lastIndex = req.Entries[len(req.Entries)-1].Index
 		}
-		n.commitIndex = min(request.LeaderCommit, lastIndex)
+		n.commitIndex = min(req.LeaderCommit, lastIndex)
 	}
 
 	response.Success = true
+	n.electionTimer = newElectionTimer() // TODO, I'm not sure if appendEntries() should be responsible for resetting the timer... but if we wait to reset outside of the function, we will need to interrogate the response to determine whether or not it was from a leader or not
 
 	return &response
 }
 
 func (n *Node) requestVote(req *RequestVoteReq) *RequestVoteResp {
-	if req.Term > n.currentTerm {
-		n.currentTerm = req.Term
-		n.role = Follower
-		n.votedFor = ""
-	}
-
 	response := RequestVoteResp{
 		Term:        n.currentTerm,
 		VoteGranted: false,
@@ -199,6 +216,12 @@ func (n *Node) requestVote(req *RequestVoteReq) *RequestVoteResp {
 	// 1. Reply false if term < currentTerm (§5.1)
 	if req.Term < n.currentTerm {
 		return &response
+	}
+
+	if req.Term > n.currentTerm {
+		n.currentTerm = req.Term
+		n.role = Follower
+		n.votedFor = ""
 	}
 
 	// 2. If votedFor is null or candidateId, and candidate’s log is at least as up-to-date as receiver’s log, grant vote (§5.2, §5.4)
@@ -223,4 +246,73 @@ func (n *Node) requestVote(req *RequestVoteReq) *RequestVoteResp {
 	response.VoteGranted = true
 
 	return &response
+}
+
+// A Node begins an election by incrementing it's current term, updating the it's role to Candidate, and voting for itself.
+// Next, the Node sends RequestVote RPCs to each of it's peers in the cluster before creating a separate Go routine processes the incoming RequestVoteResp's.
+// The Go routine notifies the orchestrator if an election has been won.
+func (n *Node) beginElection() {
+	n.currentTerm++
+	n.role = Candidate
+	n.votedFor = n.id
+
+	responses := make(chan *RequestVoteResp)
+
+	for id, peer := range n.cluster {
+		go func(id string, peer Peer) {
+			ctx, cancel := context.WithTimeout(context.Background(), MaxRPCTimeout) // TODO revisit the ctx timeout here
+			defer cancel()
+
+			resp, err := peer.RequestVote(ctx, &RequestVoteReq{
+				Term:         n.currentTerm,
+				LastLogIndex: n.log[len(n.log)-1].Index,
+				LastLogTerm:  n.log[len(n.log)-1].Term,
+				CandidateId:  n.id,
+			})
+			if err != nil {
+				// TODO log this
+				// if we get an error, we should probably send a response as false
+				// resp.VoteGranted = false
+			}
+			responses <- resp
+		}(id, peer)
+	}
+
+	go func(term uint64) {
+		defer close(responses)
+		var votesGranted int
+
+		// TODO trying to determine the best way to handle this
+		// If there are n nodes in the cluster, we only need to run this n-1 times
+		// Wait for a response to come in, or bail entirely after 300ms elapses
+		for range len(n.cluster) - 1 {
+			select {
+			case resp := <-responses:
+				// If the Node's current role is no longer Candidate, this means we've conceded to a new Leader and have reverted to follower
+				if n.role != Candidate {
+					break
+				}
+
+				// If the original term is smaller than the currentTerm, the electionTimer has expired and we've moved on to a new election.
+				if n.currentTerm > term {
+					break
+				}
+
+				if resp.VoteGranted && resp.Term == term {
+					votesGranted++
+					if votesGranted >= (len(n.cluster)/2)+1 {
+						n.electionResults <- term
+						return
+					}
+				}
+			case <-time.After(MaxRPCTimeout):
+				return
+			}
+		}
+	}(n.currentTerm)
+
+}
+
+func newElectionTimer() *time.Timer {
+	return time.NewTimer(time.Duration(rand.IntN(300-150+1)+150) * time.Millisecond)
 }
